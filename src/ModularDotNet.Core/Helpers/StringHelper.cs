4c8e81731e5bc39377e758b4c6d5d54a1c0b4eef
Kelvin Go <iam@kelvingo.com>
2020-10-25 01:38:55 +0800

    Add StringHelper

---
+++ src/ModularDotNet.Core/Helpers/StringHelper.cs
@@ -0,0 +1,11 @@
+public class StringHelper
+{
+    #region Method
+
+    public static string GenerateRandomString(int length, bool numeric = false, bool lowercase = false, bool uppercase = false, bool space = false, bool underscore = false, bool hypen = false, bool period = false)
+    {
+        return "".GenerateRandomString(length, numeric, lowercase, uppercase, space, underscore, hypen, period);
+    }
+
+    #endregion
+}
---
+++ test/ModularDotNet.Core.Tests/StringHelperTests.cs
@@ -0,0 +1,60 @@
+using System;
+using Xunit;
+
+namespace ModularDotNet.Core.Tests
+{
+    public class StringHelperTests
+    {
+        #region Fields
+
+        private const int _TestRandomRound = 10;
+
+        private static Random _Seed = new Random();
+
+        #endregion
+
+        #region Methods
+
+        [Fact]
+        public void StringHelper_GenerateRandomString_NoPrefer()
+        {
+            for (var i = 0; i < _TestRandomRound; i++)
+            {
+                var randomValue = StringHelper.GenerateRandomString(new Random(_Seed.Next()).Next(10, 100));
+                Assert.Matches(@"^[0-9a-zA-Z]+$", randomValue);
+            }
+        }
+
+        [Fact]
+        public void StringHelper_GenerateRandomString_NumberOnly()
+        {
+            for (var i = 0; i < _TestRandomRound; i++)
+            {
+                var randomValue = StringHelper.GenerateRandomString(new Random(_Seed.Next()).Next(10, 100), true);
+                Assert.Matches(@"^[0-9]+$", randomValue);
+            }
+        }
+
+        [Fact]
+        public void StringHelper_GenerateRandomString_LowercaseOnly()
+        {
+            for (var i = 0; i < _TestRandomRound; i++)
+            {
+                var randomValue = StringHelper.GenerateRandomString(new Random(_Seed.Next()).Next(10, 100), lowercase: true);
+                Assert.Matches(@"^[a-z]+$", randomValue);
+            }
+        }
+
+        [Fact]
+        public void StringHelper_GenerateRandomString_UppercaseOnly()
+        {
+            for (var i = 0; i < _TestRandomRound; i++)
+            {
+                var randomValue = StringHelper.GenerateRandomString(new Random(_Seed.Next()).Next(10, 100), uppercase: true);
+                Assert.Matches(@"^[A-Z]+$", randomValue);
+            }
+        }
+
+        #endregion
+    }
+}
