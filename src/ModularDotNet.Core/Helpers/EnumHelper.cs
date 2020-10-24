4c51dfbc07427d636a29206944e406f640754386
Kelvin Go <iam@kelvingo.com>
2020-10-25 01:49:21 +0800

    Add EnumHelper

---
+++ src/ModularDotNet.Core/Helpers/EnumHelper.cs
@@ -0,0 +1,91 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class EnumHelper
+{
+    #region Methods
+
+    public static Dictionary<int, string> GetDictionary<T>() where T : struct
+    {
+        if (!typeof(T).IsEnum)
+        {
+            throw new ArgumentException("T is not an Enum type!");
+        }
+
+        return Enum.GetValues(typeof(T))
+            .Cast<object>()
+            .ToDictionary(x => (int)x, x => x.ToString());
+    }
+
+    public static Dictionary<int, string> GetDictionary<T>(params T[] ignoreValues) where T : struct
+    {
+        if (!typeof(T).IsEnum)
+        {
+            throw new ArgumentException("T is not an Enum type!");
+        }
+
+        var ignore = ignoreValues.Cast<object>();
+
+        return Enum.GetValues(typeof(T))
+            .Cast<object>()
+            .Where(x => !ignore.Contains(x))
+            .ToDictionary(x => (int)x, x => x.ToString());
+    }
+
+    public static Dictionary<int, string> GetDictionaryInDescription<T>() where T : struct
+    {
+        if (!typeof(T).IsEnum)
+        {
+            throw new ArgumentException("T is not an Enum type!");
+        }
+
+        return Enum.GetValues(typeof(T))
+            .Cast<object>()
+            .ToDictionary(x => (int)x, x => ((Enum)x).GetDescription());
+    }
+
+    public static Dictionary<int, string> GetDictionaryInDescription<T>(params T[] ignoreValues) where T : struct
+    {
+        if (!typeof(T).IsEnum)
+        {
+            throw new ArgumentException("T is not an Enum type!");
+        }
+
+        var ignore = ignoreValues.Cast<object>();
+
+        return Enum.GetValues(typeof(T))
+            .Cast<object>()
+            .Where(x => !ignore.Contains(x))
+            .ToDictionary(x => (int)x, x => ((Enum)x).GetDescription());
+    }
+    
+    public static Dictionary<int, string> GetDictionaryInDisplayText<T>() where T : struct
+    {
+        if (!typeof(T).IsEnum)
+        {
+            throw new ArgumentException("T is not an Enum type!");
+        }
+
+        return Enum.GetValues(typeof(T))
+            .Cast<object>()
+            .ToDictionary(x => (int)x, x => ((Enum)x).GetDisplayText());
+    }
+
+    public static Dictionary<int, string> GetDictionaryInDisplayText<T>(params T[] ignoreValues) where T : struct
+    {
+        if (!typeof(T).IsEnum)
+        {
+            throw new ArgumentException("T is not an Enum type!");
+        }
+
+        var ignore = ignoreValues.Cast<object>();
+
+        return Enum.GetValues(typeof(T))
+            .Cast<object>()
+            .Where(x => !ignore.Contains(x))
+            .ToDictionary(x => (int)x, x => ((Enum)x).GetDisplayText());
+    }
+
+    #endregion
+}
---
+++ test/ModularDotNet.Core.Tests/EnumHelperTests.cs
@@ -0,0 +1,93 @@
+using System;
+using ModularDotNet.Core.Tests.Enums;
+using Xunit;
+
+namespace ModularDotNet.Core.Tests
+{
+    public class EnumHelperTests
+    {
+        #region Methods
+
+        [Fact]
+        public void EnumHelper_GetDictionary()
+        {
+            var dic = EnumHelper.GetDictionary<EnumGender>();
+
+            foreach (var each in Enum.GetValues(typeof(EnumGender)))
+            {
+                Assert.True(dic.ContainsKey((int)each) && each.ToString().Equals(dic[(int)each]));
+            }
+        }
+
+        [Fact]
+        public void EnumHelper_GetDictionary_WithIgnore()
+        {
+            var dic = EnumHelper.GetDictionary<EnumGender>(EnumGender.Male);
+
+            foreach (var each in Enum.GetValues(typeof(EnumGender)))
+            {
+                if ((EnumGender)each == EnumGender.Male)
+                {
+                    continue;
+                }
+
+                Assert.True(dic.ContainsKey((int)each) && each.ToString().Equals(dic[(int)each]));
+            }
+        }
+
+        [Fact]
+        public void EnumHelper_GetDictionaryInDescription()
+        {
+            var dic = EnumHelper.GetDictionaryInDescription<EnumGender>();
+
+            foreach (var each in Enum.GetValues(typeof(EnumGender)))
+            {
+                Assert.True(dic.ContainsKey((int)each) && ((Enum)each).GetDescription().Equals(dic[(int)each]));
+            }
+        }
+
+        [Fact]
+        public void EnumHelper_GetDictionaryInDescription_WithIgnore()
+        {
+            var dic = EnumHelper.GetDictionaryInDescription<EnumGender>(EnumGender.Male);
+
+            foreach (var each in Enum.GetValues(typeof(EnumGender)))
+            {
+                if ((EnumGender)each == EnumGender.Male)
+                {
+                    continue;
+                }
+
+                Assert.True(dic.ContainsKey((int)each) && ((Enum)each).GetDescription().Equals(dic[(int)each]));
+            }
+        }
+        [Fact]
+        public void EnumHelper_GetDictionaryInDisplayText()
+        {
+            var dic = EnumHelper.GetDictionaryInDisplayText<EnumGender>();
+
+            foreach (var each in Enum.GetValues(typeof(EnumGender)))
+            {
+                Assert.True(dic.ContainsKey((int)each) && ((Enum)each).GetDisplayText().Equals(dic[(int)each]));
+            }
+        }
+
+        [Fact]
+        public void EnumHelper_GetDictionaryInDisplayText_WithIgnore()
+        {
+            var dic = EnumHelper.GetDictionaryInDisplayText<EnumGender>(EnumGender.Male);
+
+            foreach (var each in Enum.GetValues(typeof(EnumGender)))
+            {
+                if ((EnumGender)each == EnumGender.Male)
+                {
+                    continue;
+                }
+
+                Assert.True(dic.ContainsKey((int)each) && ((Enum)each).GetDisplayText().Equals(dic[(int)each]));
+            }
+        }
+
+        #endregion
+    }
+}
